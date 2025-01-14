669f398ec18e6faa2e434eb7168703ff5efca989
rahiieno <rahienowork@gmail.com>
2025-01-14 12:39:06 -0500

    second commit updated

--- j_s_project_01.py
+++ j_s_project_01.py
@@ -13,6 +13,12 @@ def main():
     print(f"You are a student at {college}.")
     print(f"You were a student at {highschool}")
     print(f"You find {institution} more fun.")
+    correction = input("Did I get anything wrong? ")
+    if correction == "No":
+        print("Wonderful, thank you again for telling me about yourself.")
+    elif correction == "Yes":
+        corrected = input("Oh No, What did I get wrong? ")
+        print(f"Do forgive me, I will remember that better in the future.")
 
 if __name__ == "__main__":
     main()
